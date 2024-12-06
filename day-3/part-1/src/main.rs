c91d6c1cb1c86e5fb6d446c03d2bfc2b57701dd1
Simon Rolfmore <5150058+simonrolfmore@users.noreply.github.com>
2024-12-06 18:54:39 +0000

    Oh no, regexes
    Day 3 part 1, I chose to use regexes in rust because I am a bad person.

--- day-3/part-1/src/main.rs
+++ day-3/part-1/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let regex_matcher = Regex::new(r"mul\((?<first_num>\d+),(?<second_num>\d+)\)");
+    let file_contents = fs::read_to_string("./input.txt").unwrap();
+    let verbose = true;
+
+    let mut total = 0;
+
+    for regex_match in regex_matcher.unwrap().captures_iter(file_contents.as_str()) {
+        let first_num:i32 = regex_match["first_num"].parse().unwrap();
+        let second_num:i32 = regex_match["second_num"].parse().unwrap();
+
+        if verbose {
+            println!("First num: {}, second num {}", first_num, second_num);
+        }
+
+        total += first_num * second_num;
+    }
+
+    println!("Total of all mul instructions is {total}");
 }
